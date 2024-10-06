ec4a31b239c88627da6772762364895efb72c302
Shahnawaz <mskhan2048@gmail.com>
2024-10-06 16:30:20 +0530

    basic html

--- new.py
+++ new.py
@@ -1,2 +1,24 @@
-print("strawberry".count('r'))
-print("megalomantia".index('t'))
+import http.server
+import socketserver
+
+# Define the port you want to run your server on
+PORT = 8080
+
+# Define a request handler that will serve the HTML content
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        # Set the response status code to 200 (OK)
+        self.send_response(200)
+        
+        # Set the headers (Content-type for HTML)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        
+        # Write the HTML response
+        self.wfile.write(b"<html><head><title>Hello</title></head><body><h1>Hello, World!</h1></body></html>")
+
+# Create a TCPServer instance
+with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    print(f"Serving at port {PORT}")
+    # Run the server, until interrupted
+    httpd.serve_forever()
